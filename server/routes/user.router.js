125305ccde1883b3c091d0a47e329e2d12a9caca
Jessie Conley <jconley2881@gmail.com>
2020-04-15 16:10:02 -0500

    updated user router and added database name to pool

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,11 +16,14 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {  
-  const username = req.body.username;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const email = req.body.email;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
+  const queryText = 'INSERT INTO "user" (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id';
+  pool
+    .query(queryText, [first_name, last_name, email, password])
     .then(() => res.sendStatus(201))
     .catch(() => res.sendStatus(500));
 });
