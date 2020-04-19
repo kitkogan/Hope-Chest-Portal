e40ba63c76f6c25e83f099774d507d41eb510849
Kit Kogan <kkogan.dev@gmail.com>
2020-04-19 15:10:56 -0500

    modal hide/show added ot calendar component

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import Modal from "react-bootstrap/Modal";
+import {Button, Modal} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Calendar extends Component {
+
+  state = {
+    showHide: false
+  }
+
+  handleModalShowHide(){
+    this.setState({ showHide: !this.state.showHide })
+  };
+  
   render() {
     return (
       <div className="Calendar">
        <h1>Events and Calendar</h1>
-      <Modal show={true}>
-        <Modal.Header>Hi</Modal.Header>
-        <Modal.Body>asdfasdf</Modal.Body>
-        <Modal.Footer>This is the footer</Modal.Footer>
-      </Modal>
-  );
-};
+       <Modal show={this.state.showHide}>
+          <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
+          <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+          <Button variant="secondary" onClick={() => this.handleModalShowHide()}>
+              Close
+          </Button>
+          <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+              Save Changes
+          </Button>
+          </Modal.Footer>
+        </Modal>
+        <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+Display Modal</Button>
       </div>
     );
   }
