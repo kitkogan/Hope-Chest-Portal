97e84f2f2afe22aee63ec6f4424411dd9efcb427
Kit Kogan <kkogan.dev@gmail.com>
2020-04-19 17:09:37 -0500

    add nodemailer route, config, requirements

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-
+// const sendEvent = require('./routes/sendEvent.router');
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+// app.use('/api/send', sendEvent);
 
 // Serve static files
 app.use(express.static('build'));
