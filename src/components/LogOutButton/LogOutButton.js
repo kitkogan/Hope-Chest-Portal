13162fd40426dbeee343c1d6d8df542a023510a2
Melinda Nguyen <melindanguyenn@gmail.com>
2020-04-20 15:19:52 -0500

    nav bar fixed

--- src/components/LogOutButton/LogOutButton.js
+++ src/components/LogOutButton/LogOutButton.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import '../Nav/Nav.css';
 
 const LogOutButton = props => (
-  <Link
+  <Link  className="nav-link"class="col-lg"
     // This Link shows up in multiple locations and is styled differently
     // because it's styled differently depending on where it is used, the className
     // is passed to it from it's parents through React props
