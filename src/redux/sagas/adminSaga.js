48708aaf6a3f1e8ae3dc3c7d801e738b0484f1d2
Samreen Khan <khansam0430@gmail.com>
2020-04-24 11:14:29 -0500

    working on admin, not done

--- src/redux/sagas/adminSaga.js
+++ src/redux/sagas/adminSaga.js
@@ -1,21 +1,23 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import {
+    //  put, 
+     takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 // worker Saga: will be fired on "LOGIN" actions
-function* admin (action) {
+function* getAdmin (action) {
     console.log("ADMIN saga coming from admin with:", action);
-  try {
-    // const response = yield axios.get(`/form/${action.id}`);
-    // console.log("in saga with", response.data);
-    // yield put({ type: "SET_EVENT", payload: response.data });
-  } catch (error) {
-    console.log(error);
-  }
-}
- 
+    try {
+        const response = yield axios.get(`/admin/${action.payload}`);
+        console.log('in saga with', response.data);
+        // yield put({ type: "SET_DIAPER", payload: response.data });
+    } catch (error) {
+        console.log("error on adminSaga", error);
+        }
+};
 
-function* adminSaga() {
-  yield takeLatest('GET_ADMIN', admin);
+ 
+function* admin() {
+  yield takeLatest('GET_ADMIN', getAdmin);
   
 }
 
