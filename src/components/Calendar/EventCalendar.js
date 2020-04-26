0e4238381464195dce30eebf91c1b3d51e09634f
Melinda Nguyen <melindanguyenn@gmail.com>
2020-04-25 19:57:21 -0500

    modal displays multiple options

--- src/components/Calendar/EventCalendar.js
+++ src/components/Calendar/EventCalendar.js
@@ -1,105 +1,69 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
-import { Button, Modal } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 // import { render } from 'react-dom';
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "./calendar.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-import 'sweetalert2/src/sweetalert2.scss'
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 
 const localizer = momentLocalizer(moment);
 
 class EventCalendar extends Component {
   state = {
     showHide: false,
-    events: [ {
-      start: moment().toDate(),
-      end: moment()
-        .add(1, "days")
-        .toDate(),
-      title: "Some title"
-    }]
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "An Event Title",
+      },
+    ],
   };
 
-  
-  
   handleModalShowHide() {
-    console.log('modal show hide function');
+    console.log("modal show hide function");
     this.setState({ showHide: !this.state.showHide });
   }
 
-  handleSelect = ({ start, end }) => {
-    // const title = window.prompt('New Event name')
-      const title = Swal.mixin({
-        customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-      })
-      
-      title.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'No, cancel!',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.value) {
-          title.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          title.fire(
-            'Cancelled',
-            'Your imaginary file is safe :)',
-            'error'
-          )
-        }
-      })
-  }
-
-
+  handleSelect = (event) => {
+    Swal.fire({
+      title: event.title,
+      text: "You won't be able to revert this!",
+      showCancelButton: true,
+      confirmButtonText: "Edit",
+      cancelButtonText: "Unplublish",
+      reverseButtons: false,
+      showCloseButton: true,
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          title: "Edit?",
+          text: 'Click "go" to be redirected to an edit form',
+          icon: "question",
+          confirmButtonText: "Go",
+          cancelButtonText: "Cancel",
+          showCancelButton: true,
+          showCloseButton: true,
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire(
+          "Unpublished!",
+          "Your event has been successfully unpublished.",
+          "success"
+        );
+      }
+    });
+  };
 
   render() {
     return (
       <div className="Calendar">
         <h1>Events and Calendar</h1>
-        {/* <Modal show={this.state.showHide}>
-          <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button
-              variant="secondary"
-              onClick={() => this.handleModalShowHide()}
-            >
-              Close
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => this.handleModalShowHide()}
-            >
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal> */}
-        {/* <Button variant="primary" onClick={() => this.handleModalShowHide()}>
-          Display Modal
-        </Button> */}
         <Calendar
           popup
           localizer={localizer}
@@ -107,7 +71,7 @@ class EventCalendar extends Component {
           defaultView="month"
           events={this.state.events}
           // onSelectSlot={this.handleSelect}
-          onSelectEvent={event => this.handleSelect(event)}
+          onSelectEvent={(event) => this.handleSelect(event)}
           style={{ height: "100vh" }}
         />
       </div>
