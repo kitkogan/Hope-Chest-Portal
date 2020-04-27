eeb2ba9299a4d831033840ee4a44f35b8bab250b
Melinda Nguyen <melindanguyenn@gmail.com>
2020-04-26 21:41:16 -0500

    tried to loop through events

--- src/components/Calendar/EventCalendar.js
+++ src/components/Calendar/EventCalendar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import { render } from 'react-dom';
 import { Calendar, momentLocalizer } from "react-big-calendar";
@@ -15,32 +17,61 @@ const localizer = momentLocalizer(moment);
 class EventCalendar extends Component {
   state = {
     showHide: false,
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, "days").toDate(),
-        title: "An Event Title",
-      },
-    ],
+    events: this.props.reduxState.calendarReducer,
+    //   [{
+    //   start: moment().toDate(),
+    //   end: moment()
+    //     .add(1, "days")
+    //     .toDate(),
+    //   title: "Some title"
+    // }]
   };
 
+  //get events on page load
+  componentDidMount = () => {
+    this.getCalendar();
+
+    let event = this.props.reduxState.calendarReducer;
+    for (let i = 0; i < event.length; i++) {
+      event[i].start = moment.utc(event[i].start).toDate();
+      event[i].end = moment.utc(event[i].end).toDate();
+      event[i].event_name;
+      event[i].event_description;
+      event[i].event_website;
+    }
+  };
+
+  //fetching events from calendar reducer
+  getCalendar = () => {
+    console.log(
+      "in getCalendar on CALENDAR component",
+      this.props.reduxState.user.id
+    );
+    this.props.dispatch({
+      type: "GET_CALENDAR",
+      payload: this.props.reduxState.user.id,
+    });
+  };
+
+  //not in use right now but might be useful later
   handleModalShowHide() {
     console.log("modal show hide function");
     this.setState({ showHide: !this.state.showHide });
   }
 
+  //sweet alert for selected event
   handleSelect = (event) => {
     Swal.fire({
       title: event.title,
-      text: "You won't be able to revert this!",
+      text: "Event description goes here",
       showCancelButton: true,
-      confirmButtonText: "Edit",
-      cancelButtonText: "Unplublish",
+      confirmButtonText: "More Details",
+      cancelButtonText: "Unpublish",
       reverseButtons: false,
       showCloseButton: true,
     }).then((result) => {
       if (result.value) {
-        this.props.history.push("/review");
+        this.props.history.push("/admin-details");
       } else if (
         /* Read more about handling dismissals below */
         result.dismiss === Swal.DismissReason.cancel
@@ -54,18 +85,6 @@ class EventCalendar extends Component {
     });
   };
 
-  componentDidMount = () => {
-    this.getCalendar()
-  }
-
-  getCalendar = () => {
-    console.log("in getCalendar on CALENDAR component", this.props.reduxState.user.id);
-    this.props.dispatch({
-      type: "GET_CALENDAR",
-      payload: this.props.reduxState.user.id
-    });
-  };
-
   render() {
     return (
       <div className="Calendar">
@@ -75,7 +94,8 @@ class EventCalendar extends Component {
           localizer={localizer}
           defaultDate={new Date()}
           defaultView="month"
-          events={this.state.events}
+          events={this.props.reduxState.calendarReducer}
+          // onSelectSlot={this.handleSelect}
           onSelectEvent={(event) => this.handleSelect(event)}
           style={{ height: "100vh" }}
         />
