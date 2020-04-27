0648ec28623d799e944a14e802f00ede84434e75
Jessie Conley <jconley2881@gmail.com>
2020-04-27 17:42:53 -0500

    put working on review page

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -11,6 +11,7 @@ import admin from './adminReducer';
 import userEvents from "./userEventReducer";
 import adminDetails from "./AdminDetailsReducer";
 import approvalStatus from "./approvalStatusReducer";
+import newReducer from "./newReview";
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -31,6 +32,7 @@ const rootReducer = combineReducers({
   userEvents,
   adminDetails,
   approvalStatus,
+  newReducer,
 });
 
 export default rootReducer;
