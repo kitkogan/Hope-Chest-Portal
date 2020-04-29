c9ed05d2a327a4602f9a7f96c0f392af88c7b49c
Jessie Conley <jconley2881@gmail.com>
2020-04-29 15:01:11 -0500

    admin put posting to db and admin view page, get error

--- src/components/Admin/AdminDetails.js
+++ src/components/Admin/AdminDetails.js
@@ -39,6 +39,7 @@ class AdminDetails extends Component {
     promotion: "",
     other_comment: "",
     image: "",
+    approved: false,
   };
 
   componentDidMount = () => {
@@ -48,27 +49,18 @@ class AdminDetails extends Component {
   getForm = () => {
     console.log(
       "in getForm on ADMIN FORM component",
-      this.props.reduxState.user.id
+      this.props.reduxState.form.id
     );
     this.props.dispatch({
       type: "GET_ADMIN_FORM",
-      id: this.props.reduxState.user.id,
+      id: this.props.reduxState.form.id,
     });
   };
 
   handleChange = (event, typeOf) => {
-    console.log(event.target.value);
-    console.log(event.target.placeholder);
-
-    if (event.target.value === "") {
-      this.setState({
-        [typeOf]: event.target.placeholder,
-      });
-    } else {
-      this.setState({
-        [typeOf]: event.target.value,
-      });
-    }
+    this.setState({
+      [typeOf]: event.target.value,
+    });
   };
 
   updateForm = () => {
@@ -80,77 +72,92 @@ class AdminDetails extends Component {
       type: "UPDATE_ADMIN_FORM",
       payload: this.state,
       id: this.props.reduxState.adminDetails[0].id,
+      // user: { id: this.props.reduxState.user.id },
     });
-    this.setState({
-      isEditable: false,
-    });
+    // this.setState({
+    //   isEditable: false,
+    // });
     this.getForm();
   };
 
-
   edit = () => {
     console.log("editing");
-    this.setState({
-      isEditable: true,
-      contact_first_name: this.props.reduxState.adminDetails[0]
-        .contact_email
-      // contact_last_name: this.props.reduxState.review[0].contact_last_name,
-      // contact_phone: this.props.reduxState.review[0].contact_phone,
-      // contact_email: this.props.reduxState.review[0].contact_email,
-      // company_name: this.props.reduxState.review[0].company_name,
-      // company_website: this.props.reduxState.review[0].company_website,
-      // company_street: this.props.reduxState.review[0].company_street,
-      // company_city: this.props.reduxState.review[0].company_city,
-      // company_state: this.props.reduxState.review[0].company_state,
-      // company_zip: this.props.reduxState.review[0].company_zip,
-      // show_contact_option: "",
-      // event_contact_first_name: this.props.reduxState.review[0].event_contact_first_name,
-      // event_contact_last_name: this.props.reduxState.review[0].event_contact_last_name,
-      // event_contact_phone: this.props.reduxState.review[0].event_contact_phone,
-      // event_contact_email: this.props.reduxState.review[0].event_contact_email,
-      // event_name: this.props.reduxState.review[0].event_name,
-      // event_website: this.props.reduxState.review[0].event_website,
-      // event_date: this.props.reduxState.review[0].event_date,
-      // event_time: this.props.reduxState.review[0].event_time,
-      // event_location_name: this.props.reduxState.review[0].event_location_name,
-      // event_location_street: this.props.reduxState.review[0].event_location_street,
-      // event_location_city: this.props.reduxState.review[0].event_location_city,
-      // event_location_state: this.props.reduxState.review[0].event_location_state,
-      // event_location_zip: this.props.reduxState.review[0].event_location_zip,
-      // event_type: this.props.reduxState.review[0].event_type,
-      // event_description: this.props.reduxState.review[0].event_description,
-      // event_first_time: this.props.reduxState.review[0].event_first_time,
-      // fund_description: this.props.reduxState.review[0].fund_description,
-      // contribution_amount: this.props.reduxState.review[0].contribution_amount,
-      // contribution_submission: this.props.reduxState.review[0].contribution_submission,
-      // promotion: this.props.reduxState.review[0].promotion,
-      // other_comment: this.props.reduxState.review[0].other_comment,
-      // image: this.props.reduxState.review[0].other_comment
+    this.props.dispatch({
+      type: "TOGGLE_EDIT",
     });
+    this.setState({
+      contact_first_name: this.props.reduxState.adminDetails[0]
+        .contact_first_name,
+      contact_last_name: this.props.reduxState.adminDetails[0]
+        .contact_last_name,
+      contact_phone: this.props.reduxState.adminDetails[0].contact_phone,
+      contact_email: this.props.reduxState.adminDetails[0].contact_email,
+      company_name: this.props.reduxState.adminDetails[0].company_name,
+      company_website: this.props.reduxState.adminDetails[0].company_website,
+      company_street: this.props.reduxState.adminDetails[0].company_street,
+      company_city: this.props.reduxState.adminDetails[0].company_city,
+      company_state: this.props.reduxState.adminDetails[0].company_state,
+      company_zip: this.props.reduxState.adminDetails[0].company_zip,
+      show_contact_option: this.props.reduxState.adminDetails[0]
+        .show_contact_option,
+      event_contact_first_name: this.props.reduxState.adminDetails[0]
+        .event_contact_first_name,
+      event_contact_last_name: this.props.reduxState.adminDetails[0]
+        .event_contact_last_name,
+      event_contact_phone: this.props.reduxState.adminDetails[0].contact_phone,
+      event_contact_email: this.props.reduxState.adminDetails[0].contact_email,
+      event_name: this.props.reduxState.adminDetails[0].event_name,
+      event_website: this.props.reduxState.adminDetails[0].event_website,
+      event_date: this.props.reduxState.adminDetails[0].event_date,
+      event_time: this.props.reduxState.adminDetails[0].event_time,
+      event_location_name: this.props.reduxState.adminDetails[0]
+        .event_location_name,
+      event_location_street: this.props.reduxState.adminDetails[0]
+        .event_location_street,
+      event_location_city: this.props.reduxState.adminDetails[0]
+        .event_location_city,
+      event_location_state: this.props.reduxState.adminDetails[0]
+        .event_location_state,
+      event_location_zip: this.props.reduxState.adminDetails[0].event_location,
+      event_type: this.props.reduxState.adminDetails[0].event_type,
+      event_description: this.props.reduxState.adminDetails[0]
+        .event_description,
+      event_first_time: this.props.reduxState.adminDetails[0].event_first_time,
+      fund_description: this.props.reduxState.adminDetails[0].fund_description,
+      contribution_amount: this.props.reduxState.adminDetails[0]
+        .contribution_amount,
+      contribution_submission: this.props.reduxState.adminDetails[0]
+        .contribution_submission,
+      promotion: this.props.reduxState.adminDetails[0].promotion,
+      other_comment: this.props.reduxState.adminDetails[0].other_comment,
+      image: this.props.reduxState.adminDetails[0].image,
+      approved: this.props.reduxState.adminDetails[0].approved,
+    });
+
     console.log(
-      "this is state!!!!!!!!!!!",this.props.reduxState.adminDetails[0]
-        .contact_first_name
+      "this is state!!!!!!!!!!!",
+      this.props.reduxState.adminDetails[0].contact_first_name
     );
   };
 
   cancel = () => {
     console.log("cancel edits");
-    this.setState({
-      isEditable: false,
+    this.props.dispatch({
+      type: "TOGGLE_EDIT",
     });
   };
 
-  
-  goToAdmin = ()=>{
+  goToAdmin = () => {
     alert("The form has been saved!");
-    (this.props.history.push("/admin"))
+    this.props.history.push("/admin");
   };
 
   render() {
     return (
       <div className="AdminDetails">
         <h1 className="adminHead">Event Details</h1>
-        {this.state.isEditable ? (
+         {this.props.reduxState.adminReview.edit ? (
+        
           <>
             {this.props.reduxState.adminDetails.map((intake) => (
               <center>
@@ -518,10 +525,15 @@ class AdminDetails extends Component {
           </>
         )}
         <center>
-          {this.state.isEditable ? (
+          {this.props.reduxState.adminReview.edit ? (
+          
             <>
-              <button className="back" onClick={() => this.cancel()}>Cancel</button>
-              <button className="updateBtn" onClick={() => this.updateForm()}>Update</button>
+              <button className="back" onClick={() => this.cancel()}>
+                Cancel
+              </button>
+              <button className="updateBtn" onClick={() => this.updateForm()}>
+                Update
+              </button>
             </>
           ) : (
             <>
